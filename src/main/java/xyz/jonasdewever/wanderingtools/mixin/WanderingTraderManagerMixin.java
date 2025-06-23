b5f6263b0eaabefcefeb0177a1d37bace95bc55f
Jonas De Wever <dev@jonasdewever.xyz>
2025-06-23 14:37:54 +0200

    Implement a message on Wandering Trader spawn
    Includes their location in the world and notable trades as defined in the code (for now).
    Accounts for custom names

--- src/main/java/xyz/jonasdewever/wanderingtools/mixin/WanderingTraderManagerMixin.java
+++ src/main/java/xyz/jonasdewever/wanderingtools/mixin/WanderingTraderManagerMixin.java
@@ -1,4 +1,88 @@
 package xyz.jonasdewever.wanderingtools.mixin;
 
-public class WanderingTraderManagerMixin {
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.WanderingTraderEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradedItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Mixin(ServerWorld.class)
+abstract class ServerWorldMixin {
+  @Inject(method = "spawnEntity", at = @At("TAIL"))
+  private void onEntitySpawn(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+    if (entity instanceof WanderingTraderEntity wandy && cir.getReturnValue()) {
+
+      ServerWorld world = (ServerWorld) (Object) this;
+      BlockPos position = entity.getBlockPos();
+
+      boolean useName = true;
+      String wandyName = entity.getName().getString();
+      if (wandyName.equals("Wandering Trader")) useName = false;
+
+      StringBuilder message = new StringBuilder("🧉 A Trader ");
+      if (useName) message.append(String.format("%s ", wandyName));
+      message.append(String.format("has wandered into this realm at approx. X: %d, Y: %d, Z: %d\n",
+              position.getX(), position.getY(), position.getZ()));
+
+      List<TradeOffer> offers = wandy.getOffers();
+      Set<Item> itemsWeWant = Set.of(
+              Items.MOSS_BLOCK,
+              Items.PALE_MOSS_BLOCK,
+              Items.PACKED_ICE,
+              Items.BLUE_ICE,
+              Items.KELP,
+              Items.SLIME_BALL,
+              Items.POINTED_DRIPSTONE,
+              Items.SAND,
+              Items.RED_SAND,
+              Items.ROOTED_DIRT,
+              Items.PODZOL,
+              Items.ACACIA_SAPLING,
+              Items.BIRCH_SAPLING,
+              Items.CHERRY_SAPLING,
+              Items.SPRUCE_SAPLING,
+              Items.JUNGLE_SAPLING,
+              Items.DARK_OAK_SAPLING,
+              Items.OAK_SAPLING,
+              Items.PALE_OAK_SAPLING,
+              Items.MANGROVE_PROPAGULE
+      );
+      HashSet<Item> notableItems = new HashSet<>();
+
+      for (TradeOffer offer : offers) {
+        ItemStack output = offer.getSellItem();
+        if (itemsWeWant.contains(output.getItem())) {
+          notableItems.add(output.getItem());
+        }
+      }
+
+      if (notableItems.isEmpty()) {
+        message.append("\nThey are awaiting execution for their failure to present good trades.");
+      } else {
+        for (Item item : notableItems) {
+          message.append("\nBro has: ").append(item.getName().getString());
+        }
+      }
+
+      world.getServer().getPlayerManager().broadcast(
+              Text.literal(message.toString()).formatted(Formatting.GOLD),
+              false
+      );
+    }
+  }
 }
